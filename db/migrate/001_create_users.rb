f644cb0e524c80de9b87ff4c2c5f5b36a7196531
github username <github email address>
2020-03-09 19:11:52 -0500

    Added a users database

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[4.2]
             t.string :first_name
             t.string :last_name
             t.string :email
-            t.string :password_digest
+            t.string :password_digest 
         end 
     end 
 end 
